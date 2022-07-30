2ab4f6de978c6dca9f8a70e86f74117d72533465
Dean Scarff <dos@scarff.id.au>
2022-07-30 18:49:21 +1000

    Refactor TestClimbingRatings test
    Don't use setUp to copy input data, since different test cases may want
    different data.

--- tests/test_climbing_ratings.py
+++ tests/test_climbing_ratings.py
@@ -29,29 +29,31 @@ import unittest
 class TestClimbingRatings(unittest.TestCase):
     """Goldens test for climbing_ratings' main script"""
 
-    def setUp(self) -> None:
+    def copy_inputdata(self, testdata: str) -> str:
         with contextlib.ExitStack() as stack:
-            self._tmpdir = tempfile.TemporaryDirectory()
-            stack.enter_context(self._tmpdir)
+            ctx = tempfile.TemporaryDirectory()
+            stack.enter_context(ctx)
             self.addCleanup(stack.pop_all().close)
-        shutil.copy(self.get_golden("ascents.csv"), self._tmpdir.name)
-        shutil.copy(self.get_golden("pages.csv"), self._tmpdir.name)
-        shutil.copy(self.get_golden("style_pages.csv"), self._tmpdir.name)
-        shutil.copy(self.get_golden("routes.csv"), self._tmpdir.name)
+        tmpdir = ctx.name
+        shutil.copy(self.get_golden(testdata, "ascents.csv"), tmpdir)
+        shutil.copy(self.get_golden(testdata, "pages.csv"), tmpdir)
+        shutil.copy(self.get_golden(testdata, "style_pages.csv"), tmpdir)
+        shutil.copy(self.get_golden(testdata, "routes.csv"), tmpdir)
+        return tmpdir
 
-    def get_golden(self, filename: str) -> str:
+    def get_golden(self, testdata: str, filename: str) -> str:
         """Get the path to the golden copy of the given file."""
-        return os.path.join("tests", "testdata", filename)
+        return os.path.join(testdata, filename)
 
-    def assert_matches_golden(self, filename: str) -> None:
+    def assert_matches_golden(self, tmpdir: str, testdata: str, filename: str) -> None:
         """Raise an exception if the golden and generated CSVs do not match.
 
         Assumes the first row and first column can be compared as strings,
         while all other cells are compared approximately as floating points.
         """
         try:
-            fp_actual = open(os.path.join(self._tmpdir.name, filename), newline="")
-            fp = open(self.get_golden(filename), newline="")
+            fp_actual = open(os.path.join(tmpdir, filename), newline="")
+            fp = open(self.get_golden(testdata, filename), newline="")
             reader_actual = iter(csv.reader(fp_actual))
             reader = iter(csv.reader(fp))
 
@@ -77,12 +79,14 @@ class TestClimbingRatings(unittest.TestCase):
 
     def test_main(self) -> None:
         """Test climbing_ratings"""
-        cmd = [sys.executable, "-m", "climbing_ratings", self._tmpdir.name]
+        testdata = os.path.join("tests", "testdata")
+        tmpdir = self.copy_inputdata(testdata)
+        cmd = [sys.executable, "-m", "climbing_ratings", tmpdir]
         subprocess.run(cmd, check=True, stdout=subprocess.DEVNULL)
 
-        self.assert_matches_golden("page_ratings.csv")
-        self.assert_matches_golden("route_ratings.csv")
-        self.assert_matches_golden("style_page_ratings.csv")
+        self.assert_matches_golden(tmpdir, testdata, "page_ratings.csv")
+        self.assert_matches_golden(tmpdir, testdata, "route_ratings.csv")
+        self.assert_matches_golden(tmpdir, testdata, "style_page_ratings.csv")
 
     def test_main_v3(self) -> None:
         """Test climbing_ratings backwards-compatibility with v3 data"""
@@ -106,7 +110,7 @@ class TestClimbingRatings(unittest.TestCase):
             "-n",
             "--max-iterations",
             "1",
-            self._tmpdir.name,
+            os.path.join("tests", "testdata"),
         ]
         subprocess.run(cmd, check=True, stdout=subprocess.DEVNULL)
 
